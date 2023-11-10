857bea926b613c56bddd4555139b72555e0dab83
dongjae <dkekzhs5@naver.com>
2023-11-10 13:56:53 +0900

    feat : 추가 webConfig

---
+++ enjoytrip/src/main/java/com/ssafy/web/util/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.ssafy.web.util.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		// TODO Auto-generated method stub
+		registry.addMapping("/**")
+		.allowedOrigins("http://localhost:5173")
+		.allowedMethods("POST","GET")
+		.allowCredentials(true);
+	}
+
+	
+}
