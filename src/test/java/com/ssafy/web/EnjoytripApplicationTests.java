905e666c30615d8d65d9ac82b3a4dc6d04d832f4
dongjae <dkekzhs5@naver.com>
2023-11-10 12:17:50 +0900

    init

---
+++ src/test/java/com/ssafy/web/EnjoytripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnjoytripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
