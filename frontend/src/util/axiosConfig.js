8c0461c0afa30c638fcbda684b68bf3724c20302
louise <wizardchw0@gmail.com>
2023-11-13 13:30:20 +0900

    readme 수정

--- frontend/src/util/axiosConfig.js
+++ frontend/src/util/axiosConfig.js
@@ -3,9 +3,9 @@ import axios from "axios";
 axios.defaults.withCredentials = true; // withCredentials 전역 설정
 
 const apiClient = axios.create({
-    baseURL: "http://localhost:80/enjoytrip",
-    headers: {
-        "Content-Type": "application/json;charset=utf-8",
-    },
+  baseURL: "http://localhost:8080/enjoytrip",
+  headers: {
+    "Content-Type": "application/json;charset=utf-8",
+  },
 });
-export default apiClient
+export default apiClient;
