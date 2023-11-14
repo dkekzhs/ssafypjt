5790674a1aa18a7eafaa5050227ad6c0752b8ec5
dongjae <dkekzhs5@naver.com>
2023-11-14 13:34:28 +0900

    travel 시도 구군 가져오기

---
+++ frontend/src/util/travel/travelApi.js
@@ -0,0 +1,16 @@
+import apiClient from "@/util/axiosConfig";
+
+function getSido(success, fail) {
+  apiClient.post("/travel/search/getSidoList").then(success).catch(fail);
+}
+
+function getType(success, fail) {
+  apiClient.post("/travel/search/getTypeList").then(success).catch(fail);
+}
+
+function getGugun(body, success, fail) {
+  apiClient.post("/travel/search/getGugunList", body).then(success).catch(fail);
+}
+
+
+export { getSido, getType,getGugun };
